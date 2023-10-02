33c211db10a1bfa41cd70bd5eb30037709b974d2
JanAnders <jan@janders.net>
2023-10-02 02:10:54 +0200

    create processed dataset, add to dvc

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,6 +2,9 @@
 import click
 import logging
 from pathlib import Path
+import pandas as pd
+import os 
+
 from dotenv import find_dotenv, load_dotenv
 
 
@@ -14,6 +17,38 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    datasets = []
+
+    for file in os.listdir("./data/raw/"):
+        if ".csv" in file:
+            data = pd.read_csv("./data/raw/"+file, sep = ",", quoting=False)
+            datasets.append(data)
+
+    data = pd.concat(datasets)
+    data.shape
+
+
+    ic_trains = data[~data['TIN'].isna() & data['TIN'].str.contains('IC')]
+    ic_trains.shape
+
+
+    ### Add some time features
+
+    #Month
+    ic_trains.loc[:,"MONTH"] = pd.to_datetime(ic_trains["TID"], format='%d.%m.%y').dt.month
+    # Day of Week
+    ic_trains.loc[:,"DOW"] = pd.to_datetime(ic_trains["TID"], format='%d.%m.%y').dt.dayofweek
+    # Day
+    ic_trains.loc[:,"DAY"] = pd.to_datetime(ic_trains["TID"], format='%d.%m.%y').dt.day
+    # Hour
+    ic_trains.loc[:,"HOUR"] = pd.to_datetime(ic_trains["TIT"], format='%H:%M').dt.hour
+    ic_trains.loc[:,"MIN"] = pd.to_datetime(ic_trains["TIT"], format='%H:%M').dt.minute
+
+
+    final_dataset = ic_trains[["TIN","TIR","TIM","TIRE","TIP","TSC","TSC","TAc",
+                            "MONTH","DOW","DAY","HOUR","MIN"]]
+
+    final_dataset.to_csv("./data/processed/data.csv")
 
 
 if __name__ == '__main__':
