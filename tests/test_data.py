2fb773dd5e60837a1e02a1ef84b4aea441bc8e4d
Maya Köhnen <m.koehnen@campus.lmu.de>
2023-10-07 22:04:02 +0200

    added test data, typing and new style format

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,58 +2,50 @@ import os
 import os.path
 
 import pandas as pd
+from typing import List
 import pytest
 import yaml
 
 from src.models.train_model import load_data, normalize_data
 from tests import _PROJECT_ROOT
+from tests.define_test_data import get_test_data
 
 
-def get_cfg():
-    with open(os.path.join(_PROJECT_ROOT,
-                           "src/configs/config.yaml"), "r") as yaml_file:
+def get_cfg() -> dict:
+    with open(os.path.join(_PROJECT_ROOT, "src/configs/config.yaml"), "r") as yaml_file:
         cfg = yaml.safe_load(yaml_file)
 
     return cfg
 
 
-@pytest.mark.skipif(not
-                    os.path.exists(get_cfg()["paths"]["training_data_path"]),
-                    reason="Data files not found")
+@pytest.mark.skipif(
+    not os.path.exists(get_cfg()["paths"]["training_data_path"]),
+    reason="Data files not found",
+)
 def test_data_shape():
     cfg = get_cfg()
     data = load_data(cfg["paths"]["training_data_path"])
-    assert len(data) == 66595, \
-        "Dataset did not have the correct number of samples"
-    assert data.shape[1] == cfg["hyperparameters"]["input_size"] + 1, \
-        "Dataset did not have the correct number of columns"
+    assert len(data) == 66595, "Dataset did not have the correct number of samples"
+    assert (
+        data.shape[1] == cfg["hyperparameters"]["input_size"] + 1
+    ), "Dataset did not have the correct number of columns"
 
 
-@pytest.mark.skipif(not
-                    os.path.exists(get_cfg()["paths"]["training_data_path"]),
-                    reason="Data files not found")
 def test_x_y_split():
     cfg = get_cfg()
-    data = load_data(cfg["paths"]["training_data_path"])
+    data = get_test_data()
     x, y = normalize_data(data)
-    assert x.shape[1] == cfg["hyperparameters"]["input_size"], \
-        "Features do not have the correct shape"
-    assert len(y.shape) == cfg["hyperparameters"]["output_size"], \
-        "Targets do not have the correct shape"
-    assert y.min() >= -1437 and y.max() <= 156, "Targets not in expected range"
+    assert x.shape[1] == cfg["hyperparameters"]["input_size"], "Features do not have the correct shape"
+    assert len(y.shape) == cfg["hyperparameters"]["output_size"], "Targets do not have the correct shape"
 
 
-@pytest.mark.skipif(not
-                    os.path.exists(get_cfg()["paths"]["training_data_path"]),
-                    reason="Data files not found")
 def test_normalization():
-    cfg = get_cfg()
-    data = load_data(cfg["paths"]["training_data_path"])
+    data = get_test_data()
     x, _ = normalize_data(data)
 
-    x_vals = []
+    x_vals: List[list] = []
     for x_i in x:
         x_vals.extend(x_i.tolist())
-    assert all((pd.isna(x_val) or (x_val >= 0 and x_val <= 1))
-               for x_val in x_vals), \
-        "Features are not normalized correctly"
+    assert all(
+        (pd.isna(x_val) or (x_val >= 0 and x_val <= 1)) for x_val in x_vals
+    ), "Features are not normalized correctly"
--- tests/test_model.py
+++ tests/test_model.py
@@ -6,13 +6,13 @@ import torch
 import yaml
 
 from src.models.model import LightningModel
-from src.models.train_model import load_data, normalize_data
 from tests import _PROJECT_ROOT
 
+from tests.define_test_data import get_normalized_test_data
 
-def get_hparams():
-    with open(os.path.join(_PROJECT_ROOT,
-                           "src/configs/config.yaml"), "r") as yaml_file:
+
+def get_hparams() -> dict:
+    with open(os.path.join(_PROJECT_ROOT, "src/configs/config.yaml"), "r") as yaml_file:
         cfg = yaml.safe_load(yaml_file)
 
     hparams = {"lr": cfg["hyperparameters"]["learning_rate"],
@@ -29,42 +29,46 @@ def get_hparams():
     return hparams
 
 
-def get_paths():
-    with open(os.path.join(_PROJECT_ROOT,
-                           "src/configs/config.yaml"), "r") as yaml_file:
+def get_paths() -> dict:
+    with open(os.path.join(_PROJECT_ROOT, "src/configs/config.yaml"), "r") as yaml_file:
         cfg = yaml.safe_load(yaml_file)
 
-    paths = {"training_data_path": cfg["paths"]["training_data_path"], }
+    paths = {
+        "training_data_path": cfg["paths"]["training_data_path"],
+    }
     return paths
 
 
-@pytest.mark.skipif(not os.path.exists(get_paths()["training_data_path"]),
-                    reason="Data files not found")
+@pytest.mark.skipif(not os.path.exists(get_paths()["training_data_path"]), reason="Data files not found")
 def test_model_output():
-    data = load_data(get_paths()["training_data_path"])
-    x, _ = normalize_data(data)
+    x, _ = get_normalized_test_data()
 
     hparams = get_hparams()
 
     model = LightningModel(hparams)
     y_pred = model(x[:1])
 
-    assert y_pred.shape[0] == hparams["output_size"], \
-        "Model output has not the correct shape"
+    assert y_pred.shape[0] == hparams["output_size"], "Model output has not the correct shape"
 
 
 @pytest.mark.parametrize(
     "test_input,expectation",
-    [(torch.randn(1, 90), does_not_raise()),
-     (torch.randn(1, 2, 3), pytest.raises(
-         ValueError,
-         match='Expected input to a 2D tensor')),
-     (torch.randn(20, 30), pytest.raises(
-         ValueError,
-         match=r'Expected each sample to have shape'
-         + rf'\[{get_hparams()["input_size"]}\]')),
-     ],)
-def test_error_on_wrong_shape(test_input, expectation):
+    [
+        (torch.randn(1, 90), does_not_raise()),
+        (
+            torch.randn(1, 2, 3),
+            pytest.raises(ValueError, match="Expected input to a 2D tensor"),
+        ),
+        (
+            torch.randn(20, 30),
+            pytest.raises(
+                ValueError,
+                match=r"Expected each sample to have shape" + rf'\[{get_hparams()["input_size"]}\]',
+            ),
+        ),
+    ],
+)
+def test_error_on_wrong_shape(test_input: torch.Tensor, expectation: any):
     hparams = get_hparams()
     model = LightningModel(hparams)
 
