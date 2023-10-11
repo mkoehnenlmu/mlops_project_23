bab01a03a916268555d0d548468098c25b9ce5de
Maya Köhnen <m.koehnen@campus.lmu.de>
2023-10-11 10:05:10 +0200

    updated monitoring to work with new data and new db structure

--- src/visualization/monitoring.py
+++ src/visualization/monitoring.py
@@ -23,14 +23,13 @@ from evidently.tests import (
 )
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from google.cloud import storage
 
 # from enum import Enum
 from src.data.load_data import get_additional_configs, get_paths, load_data
 from src.models.model import LightningModel
 from src.models.predict_model import load_model
 
-LOCAL = True  # set this to true when developing locally
+LOCAL = False  # set this to true when developing locally
 
 
 app = FastAPI()
@@ -46,10 +45,10 @@ def root():
     return response
 
 
-def load_reference_data(from_remote: bool = not LOCAL):
+def load_reference_data(from_remote: bool = not LOCAL, column_set: tuple = None, sample_size: int = 1000,):
     reference_data = load_data(n_rows=10000)
     # sample 1000 rows from reference data
-    reference_data = reference_data.sample(100)
+    reference_data = reference_data.sample(sample_size)
     reference_prediction = reference_data.pop(get_additional_configs()["dependent_var"])
     reference_data.insert(
         reference_data.shape[1],
@@ -58,44 +57,23 @@ def load_reference_data(from_remote: bool = not LOCAL):
     )
 
     if from_remote:
-        storage_client = storage.Client()
-        bucket = storage_client.get_bucket(get_paths()["inference_bucket"])
-        # open the file "database.csv" from the bucket add a new to to the csv, the upload again
-        blob = bucket.blob(
-            get_paths()["inference_data_path"].split["/"][1]
-            + get_paths()["inference_data_path"].split["/"][2]
-        )
-        blob.download_to_filename(get_paths()["inference_data_path"])
-    current_data = pd.read_csv(get_paths()["inference_data_path"])
+        current_data = load_data(get_paths()["inference_data_path"], get_paths()["inference_bucket"])
+    else:
+        current_data = pd.read_csv(get_paths()["inference_data_path"])
     current_data.drop(columns=["time"], inplace=True)
-
-    current_data = pd.concat(
-        [
-            current_data["input_data"]
-            .str.strip("[")
-            .str.strip("]")
-            .str.split(",", expand=True),
-            current_data["prediction"],
-        ],
-        axis=1,
-    )
     current_data.columns = reference_data.columns
-    # TODO: dtypes are "object" which is not convertible to int
+
     # convert type of columns of current_data to the same type as reference_data if not nan
     for col in current_data.columns:
         try:
-            # current_data[col] = current_data[col].astype(str)
-            # pd.to_numeric(current_data[col], errors='raise', downcast=reference_data[col].dtype)
             current_data[col] = current_data[col].astype(reference_data[col].dtype)
         except Exception:
             print(f"problem with dtype of col {col}")
 
-    print(current_data.dtypes)
-
-    return reference_data, current_data
-
-
-load_reference_data()
+    if column_set:
+        return reference_data.iloc[:, column_set[0]:column_set[1]], current_data.iloc[:, column_set[0]:column_set[1]]
+    else:
+        return reference_data, current_data
 
 
 def predict_row(row: pd.Series, model: LightningModel):
@@ -103,10 +81,7 @@ def predict_row(row: pd.Series, model: LightningModel):
     input_tensor = input_tensor.view(1, -1)
     with torch.no_grad():
         prediction = model.forward(input_tensor)
-    # if prediction.item() >= 0.5:
-    #    return 1
-    # else:
-    #    return 0
+
     return prediction.item()
 
 
@@ -126,8 +101,8 @@ def load_test_data():
 @app.get("/monitoring/", response_class=HTMLResponse)
 async def monitoring():
     """Simple get request method that returns a monitoring report."""
-    reference_data, current_data = load_reference_data()
-
+    reference_data, current_data = load_reference_data(column_set=(0, 10))
+    # get only first 10 columns of reference and current
     data_drift_report = Report(
         metrics=[
             DataDriftPreset(),
